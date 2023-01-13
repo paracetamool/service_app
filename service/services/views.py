4b36a5e61d8f38f543afcb3d6532fd736ecb81de
paracetamool <brom.1997@mail.ru>
2023-01-13 09:01:03 +0300

    added plan in api/subscriptions

--- service/services/views.py
+++ service/services/views.py
@@ -9,6 +9,7 @@ from .serializers import SubscriptionSerializers
 
 class SubscriptionView(ReadOnlyModelViewSet):
     queryset = Subscription.objects.all().prefetch_related(
+        'plan',
         Prefetch('client', 
         queryset=Client.objects.all().select_related("user").only('company_name', 'user__email'))
     )
