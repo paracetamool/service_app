20a2bf3cfe20dd97c6bed384a8e2502a41612c0c
paracetamool <brom.1997@mail.ru>
2023-01-13 14:27:35 +0300

    added task in celler and 2 workers in docker-compose

--- service/services/models.py
+++ service/services/models.py
@@ -2,11 +2,23 @@ from django.db import models
 from django.core.validators import MaxValueValidator
 
 from clients.models import Client
+from .tasks import set_ptice
 
 
 class Service(models.Model):
     name = models.CharField(max_length=50)
     full_price = models.PositiveIntegerField()
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__full_price = self.full_price
+
+    def save(self, *args,  **kwargs):
+        if self.__full_price != self.full_price:
+            for subscription in self.subscriptions.all():
+                set_ptice.delay(subscription.id)
+
+        return super().save(*args, **kwargs)
 
 
 class Plan(models.Model):
@@ -20,7 +32,23 @@ class Plan(models.Model):
     discount_percent = models.PositiveIntegerField(default=0, validators=[MaxValueValidator(100)])
 
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__discount_percent = self.discount_percent
+
+    def save(self, *args,  **kwargs):
+        if self.__discount_percent != self.discount_percent:
+            for subscription in self.subscriptions.all():
+                set_ptice.delay(subscription.id)
+
+        return super().save(*args, **kwargs)
+
+
+
+
+
 class Subscription(models.Model):
     client = models.ForeignKey(Client, related_name="subscriptions", on_delete=models.PROTECT)
     service = models.ForeignKey(Service, related_name="subscriptions", on_delete=models.PROTECT)
     plan = models.ForeignKey(Plan, related_name="subscriptions", on_delete=models.PROTECT)
+    price = models.PositiveIntegerField(default=0)
